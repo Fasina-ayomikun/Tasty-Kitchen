589e9d85a04da24aa9e756d24d33496f37857727
Fasina Ayomikun <101244317+Fasina-ayomikun@users.noreply.github.com>
2022-06-18 17:01:34 +0400

    Update Error.js

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 function Error() {
-  return <div>Error</div>;
+  return (
+    <Wrapper>
+      <h1>404</h1>
+      <button className='error-btn'>
+        <Link to='/'>Back Home</Link>
+      </button>
+    </Wrapper>
+  );
 }
-
+const Wrapper = styled.section`
+  height: calc(100vh - 90px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  h1 {
+    font-size: 15rem;
+    font-weight: 600;
+    margin: 0 auto;
+  }
+  @media (max-width: 500px) {
+    h1 {
+      font-weight: 5rem;
+    }
+  }
+`;
 export default Error;
